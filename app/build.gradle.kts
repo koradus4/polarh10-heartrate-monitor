3a43903c13e9edce237a1e52b22f959deaa1f096
koradus4 <konradborkowski25@gmail.com>
2025-11-27 23:15:45 +0100

    Bump version to v1.0.9 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "pl.fitness.polarh10"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "v1.0.8"
+        versionCode = 10
+        versionName = "v1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
